1e4adef72a77f3e6e6efc6db67bd33e70807c321
Nicolas <nicomartin854@gmail.com>
2022-09-26 22:53:39 -0300

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,9 @@
+const Formulario = () => {
+  return (
+    <>
+      <h1>Desde el formulario</h1>
+    </>
+  );
+};
+
+export default Formulario;
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,9 @@
+const ListadoPacientes = () => {
+  return (
+    <>
+      <h2>Desde listado pacientes</h2>
+    </>
+  );
+};
+
+export default ListadoPacientes;
